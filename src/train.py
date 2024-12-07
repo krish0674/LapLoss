abe602684efed96c18d483b1e8ccd5c76a2f8ea9
krish0674 <118186942+krish0674@users.noreply.github.com>
2024-12-07 11:28:45 +0530

    hi

--- src/train.py
+++ src/train.py
@@ -10,19 +10,17 @@ def main(args):
         'epochs': args.epochs,
         'dset' : args.dset,
         'device':args.device,
-        'kernel_loss_weight':args.kernel_loss_weight,
         'lr': args.lr,
         'loss_weight':args.loss_weight,
         'gan_type':args.gan_type,
-        'use_hypernet': args.use_hypernet,
         'sf_path' : args.sf_path,
         'nrb_top' :args.nrb_top,
         'nrb_high' : args.nrb_high,
         'nrb_low' : args.nrb_low
     }
     wandb.login(key=args.key)
-    wandb.init(project="HyperLPTN",
-               config={'use_hypernet':args.use_hypernet,'lr':args.lr, 'max_ssim':0, 'max_psnr':0,'test_psnr':0,'test_ssim': 0, 'best_epoch':0, 'loss_weight':args.loss_weight, 'gan_type': args.gan_type}, allow_val_change=True)
+    wandb.init(project="LapLoss",
+               config={'lr':args.lr, 'max_ssim':0, 'max_psnr':0,'test_psnr':0,'test_ssim': 0, 'best_epoch':0, 'loss_weight':args.loss_weight, 'gan_type': args.gan_type}, allow_val_change=True)
     train_model(config)
 
 if __name__ == '__main__':
@@ -34,9 +32,7 @@ if __name__ == '__main__':
     parser.add_argument('--dset', type=str, required=False, default='grad')
     parser.add_argument('--lr', type=float, required=False, default=1e-4)
     parser.add_argument('--loss_weight', type=float, required=False, default=3000)
-    parser.add_argument('--kernel_loss_weight', type=float, required=False, default=0.05)
     parser.add_argument('--gan_type', type=str, required=False)
-    parser.add_argument('--use_hypernet', type=bool, required=False, default=True)
     parser.add_argument('--key', type=str, required=False, default='3adf824888485fb1de047a4e9bab54143ddf0cd9')
     parser.add_argument('--sf_path', type=str, required=False, default='./best_model_g.pth')
     parser.add_argument('--nrb_low', type=int, required=False)
