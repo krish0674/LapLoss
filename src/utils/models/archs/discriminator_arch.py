14d7b3b4beeac4203e27619ff15e722702bf073d
krish0674 <118186942+krish0674@users.noreply.github.com>
2024-12-16 16:12:46 +0530

    hi

--- src/utils/models/archs/discriminator_arch.py
+++ src/utils/models/archs/discriminator_arch.py
@@ -7,9 +7,9 @@ def discriminator_block(in_filters, out_filters, normalization=False):
         layers.append(nn.InstanceNorm2d(out_filters, affine=True))
     return layers
 
-class Discriminator(nn.Module):
+class Discriminator1(nn.Module):
     def __init__(self):
-        super(Discriminator, self).__init__()
+        super(Discriminator1, self).__init__()
 
         self.model = nn.Sequential(
             nn.Conv2d(3, 16, 3, stride=2, padding=1),
@@ -26,3 +26,40 @@ class Discriminator(nn.Module):
     def forward(self, img_input):
         return self.model(img_input)
     
+class Discriminator2(nn.Module):
+    def __init__(self):
+        super(Discriminator2, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 16, 3, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            nn.InstanceNorm2d(16, affine=True),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
+            *discriminator_block(128, 128),
+            nn.Conv2d(128, 1, 10, padding=0),
+            nn.AdaptiveAvgPool2d((1,1))
+        )
+
+    def forward(self, img_input):
+        return self.model(img_input)
+    
+class Discriminator3(nn.Module):
+    def __init__(self):
+        super(Discriminator3, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 16, 3, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            nn.InstanceNorm2d(16, affine=True),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
+            # *discriminator_block(128, 128),
+            nn.Conv2d(128, 1, 15, padding=0),
+            nn.AdaptiveAvgPool2d((1,1))
+        )
+
+    def forward(self, img_input):
+        return self.model(img_input)
