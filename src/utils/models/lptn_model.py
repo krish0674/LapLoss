97e57e0b5e9864190a2cf87d95ba6761c0545d6f
krish0674 <118186942+krish0674@users.noreply.github.com>
2024-12-16 21:07:00 +0530

    lptn

--- src/utils/models/lptn_model.py
+++ src/utils/models/lptn_model.py
@@ -10,7 +10,9 @@ from utils.models.base_model import BaseModel
 from utils.models.losses import compute_gradient_penalty
 
 from torch import nn
-from .archs.LPTN_paper_arch import LPTNPaper,LPTNPaper1
+from .archs.LPTN_paper_arch import LPTNPaper
+from .archs.lptn import LPTN
+
 from .archs.LPTN_paper_arch import Lap_Pyramid_Conv
 from .archs.discriminator_arch import Discriminator1,Discriminator2,Discriminator3
 from .losses.losses import MSELoss, GANLoss
@@ -40,7 +42,7 @@ class LPTNModel(BaseModel):
         disc3 = disc3.to(self.device)
 
         # creating model object
-        model = LPTNPaper1(
+        model = LPTN(
         nrb_low =self.nrb_low,
         nrb_high =self.nrb_high,
         # nrb_top =self.nrb_top,
