ea9bab4245cfcf8253da327d85a34cb9deabf90b
krish0674 <118186942+krish0674@users.noreply.github.com>
2025-01-21 12:06:53 +0530

    hi

--- src/utils/models/lptn_model.py
+++ src/utils/models/lptn_model.py
@@ -261,10 +261,10 @@ class LPTNModel(BaseModel):
 
         # Loop through each discriminator
         for i, (discriminator, optimizer, pyr_gt, pyr_pred) in enumerate(zip(discriminators, optimizers, pyr_gt_levels, pyr_pred_levels)):
-            pyr_gt = pyr_gt.detach()
-            pyr_pred = pyr_pred.detach()
-            pyr_gt = (pyr_gt - pyr_gt.mean()) / (pyr_gt.std() + 1e-8)
-            pyr_pred = (pyr_pred - pyr_pred.mean()) / (pyr_pred.std() + 1e-8)  
+            # pyr_gt = pyr_gt.detach()
+            # pyr_pred = pyr_pred.detach()
+            # pyr_gt = (pyr_gt - pyr_gt.mean()) / (pyr_gt.std() + 1e-8)
+            # pyr_pred = (pyr_pred - pyr_pred.mean()) / (pyr_pred.std() + 1e-8)  
             # Real
             real_d_pred = discriminator(pyr_gt)
             l_d_real = self.GLoss(real_d_pred, True, is_disc=True)
