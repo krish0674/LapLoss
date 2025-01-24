fcccc38987686408151ce6748951766823599d4c
krish0674 <118186942+krish0674@users.noreply.github.com>
2025-01-24 22:06:07 +0530

    hi_grad

--- src/utils/evaluater.py
+++ src/utils/evaluater.py
@@ -121,50 +121,7 @@ def eval(root_dir, lr,loss_weight = 2000,gan_type = 'standard' ,device='cuda', n
     # print(f'TEST MSSIM under {mssim_test}')
 
     # Initialize the test dataset
-    test_dataset = SICEMixTest(
-        root_dir=r'/kaggle/input/sice-grad-and-sice-mix/SICEGM',
-    )
-
-    # Create the DataLoader
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
-
-    lptn_model = LPTNModel(loss_weight, device, lr, gan_type=gan_type, nrb_high=nrb_high, nrb_low=nrb_low, nrb_top=nrb_top,levels=[0,1,2],weights=[0.5,0.3,0.2])
-    # summary(lptn_model.net_g , input_size=(3, 608, 896))
-    total_loss = []
-    psnr_test,ssim_test, lpips_test,mssim_test = 0,0,0,0
-
- 
-    with tqdm(
-        test_loader
-    ) as loader:
-
-        lptn_model.load_network(path, device=device)
-        for iteration,batch_data in enumerate(loader):
-            x,y = batch_data
-            
-            lptn_model.net_g.eval()
-            
-            lptn_model.feed_data(x,y)
-            
-            psnr_test_iter,ssim_test_iter, lpips_test_iter,mssim_iter_test = lptn_model.optimize_parameters(iteration,mode='test')
-            # lptn_model.visualise(iteration=iteration)
-            flag = 0
-            
-            lpips_test += lpips_test_iter
-            psnr_test += psnr_test_iter
-            ssim_test += ssim_test_iter
-            mssim_test+=mssim_iter_test
-    lpips_test /=(iteration+1)
-    psnr_test /= (iteration+1)
-    ssim_test /= (iteration+1)
-    mssim_test /= (iteration+1)
-
-    print(f'TEST LPIPS mix {lpips_test}')
-    print(f'TEST PSNR mix  {psnr_test}')
-    print(f'TEST SSIM mix {ssim_test}')
-    print(f'TEST MSSIM mix {mssim_test}')
-
-    # test_dataset = SICEGradTest(
+    # test_dataset = SICEMixTest(
     #     root_dir=r'/kaggle/input/sice-grad-and-sice-mix/SICEGM',
     # )
 
@@ -202,10 +159,53 @@ def eval(root_dir, lr,loss_weight = 2000,gan_type = 'standard' ,device='cuda', n
     # ssim_test /= (iteration+1)
     # mssim_test /= (iteration+1)
 
-    # print(f'TEST LPIPS grad {lpips_test}')
-    # print(f'TEST PSNR grad  {psnr_test}')
-    # print(f'TEST SSIM grad {ssim_test}')
-    # print(f'TEST MSSIM grad {mssim_test}')
+    # print(f'TEST LPIPS mix {lpips_test}')
+    # print(f'TEST PSNR mix  {psnr_test}')
+    # print(f'TEST SSIM mix {ssim_test}')
+    # print(f'TEST MSSIM mix {mssim_test}')
+
+    test_dataset = SICEGradTest(
+        root_dir=r'/kaggle/input/sice-grad-and-sice-mix/SICEGM',
+    )
+
+    # Create the DataLoader
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+    lptn_model = LPTNModel(loss_weight, device, lr, gan_type=gan_type, nrb_high=nrb_high, nrb_low=nrb_low, nrb_top=nrb_top,levels=[0,1,2],weights=[0.5,0.3,0.2])
+    # summary(lptn_model.net_g , input_size=(3, 608, 896))
+    total_loss = []
+    psnr_test,ssim_test, lpips_test,mssim_test = 0,0,0,0
+
+ 
+    with tqdm(
+        test_loader
+    ) as loader:
+
+        lptn_model.load_network(path, device=device)
+        for iteration,batch_data in enumerate(loader):
+            x,y = batch_data
+            
+            lptn_model.net_g.eval()
+            
+            lptn_model.feed_data(x,y)
+            
+            psnr_test_iter,ssim_test_iter, lpips_test_iter,mssim_iter_test = lptn_model.optimize_parameters(iteration,mode='test')
+            # lptn_model.visualise(iteration=iteration)
+            flag = 0
+            
+            lpips_test += lpips_test_iter
+            psnr_test += psnr_test_iter
+            ssim_test += ssim_test_iter
+            mssim_test+=mssim_iter_test
+    lpips_test /=(iteration+1)
+    psnr_test /= (iteration+1)
+    ssim_test /= (iteration+1)
+    mssim_test /= (iteration+1)
+
+    print(f'TEST LPIPS grad {lpips_test}')
+    print(f'TEST PSNR grad  {psnr_test}')
+    print(f'TEST SSIM grad {ssim_test}')
+    print(f'TEST MSSIM grad {mssim_test}')
 
 def eval_model(configs):
     eval(configs['root_dir'],
