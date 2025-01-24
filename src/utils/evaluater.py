05f93a8c5300b73bbe07d019c7fca9a940418752
krish0674 <118186942+krish0674@users.noreply.github.com>
2025-01-24 22:04:41 +0530

    hi_mix

--- src/utils/evaluater.py
+++ src/utils/evaluater.py
@@ -75,54 +75,10 @@ def eval(root_dir, lr,loss_weight = 2000,gan_type = 'standard' ,device='cuda', n
     ]
 
     # Initialize the test dataset
-    test_dataset = SICETestDataset(
-        root_dir=r'/kaggle/input/sicedataset',
-        exposure_type='under',
-        indices=testing_indices
-    )
-
-    # Create the DataLoader
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
-
-    lptn_model = LPTNModel(loss_weight, device, lr, gan_type=gan_type, nrb_high=nrb_high, nrb_low=nrb_low, nrb_top=nrb_top,levels=[0,1,2],weights=[0.5,0.3,0.2])
-    # summary(lptn_model.net_g , input_size=(3, 608, 896))
-    total_loss = []
-    psnr_test,ssim_test, lpips_test,mssim_test = 0,0,0,0
-
- 
-    with tqdm(
-        test_loader
-    ) as loader:
-
-        lptn_model.load_network(path, device=device)
-        for iteration,batch_data in enumerate(loader):
-            x,y = batch_data
-            
-            lptn_model.net_g.eval()
-            
-            lptn_model.feed_data(x,y)
-            
-            psnr_test_iter,ssim_test_iter, lpips_test_iter,mssim_iter_test = lptn_model.optimize_parameters(iteration,mode='test')
-            lptn_model.visualise(iteration=iteration)
-            flag = 0
-            
-            lpips_test += lpips_test_iter
-            psnr_test += psnr_test_iter
-            ssim_test += ssim_test_iter
-            mssim_test+=mssim_iter_test
-    lpips_test /=(iteration+1)
-    psnr_test /= (iteration+1)
-    ssim_test /= (iteration+1)
-    mssim_test /= (iteration+1)
-
-    print(f'TEST LPIPS under {lpips_test}')
-    print(f'TEST PSNR under  {psnr_test}')
-    print(f'TEST SSIM under {ssim_test}')
-    print(f'TEST MSSIM under {mssim_test}')
-
-    # # Initialize the test dataset
-    # test_dataset = SICEMixTest(
-    #     root_dir=r'/kaggle/input/sice-grad-and-sice-mix/SICEGM',
+    # test_dataset = SICETestDataset(
+    #     root_dir=r'/kaggle/input/sicedataset',
+    #     exposure_type='under',
+    #     indices=testing_indices
     # )
 
     # # Create the DataLoader
@@ -147,7 +103,7 @@ def eval(root_dir, lr,loss_weight = 2000,gan_type = 'standard' ,device='cuda', n
     #         lptn_model.feed_data(x,y)
             
     #         psnr_test_iter,ssim_test_iter, lpips_test_iter,mssim_iter_test = lptn_model.optimize_parameters(iteration,mode='test')
-    #         # lptn_model.visualise(iteration=iteration)
+    #         lptn_model.visualise(iteration=iteration)
     #         flag = 0
             
     #         lpips_test += lpips_test_iter
@@ -159,10 +115,54 @@ def eval(root_dir, lr,loss_weight = 2000,gan_type = 'standard' ,device='cuda', n
     # ssim_test /= (iteration+1)
     # mssim_test /= (iteration+1)
 
-    # print(f'TEST LPIPS mix {lpips_test}')
-    # print(f'TEST PSNR mix  {psnr_test}')
-    # print(f'TEST SSIM mix {ssim_test}')
-    # print(f'TEST MSSIM mix {mssim_test}')
+    # print(f'TEST LPIPS under {lpips_test}')
+    # print(f'TEST PSNR under  {psnr_test}')
+    # print(f'TEST SSIM under {ssim_test}')
+    # print(f'TEST MSSIM under {mssim_test}')
+
+    # Initialize the test dataset
+    test_dataset = SICEMixTest(
+        root_dir=r'/kaggle/input/sice-grad-and-sice-mix/SICEGM',
+    )
+
+    # Create the DataLoader
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+    lptn_model = LPTNModel(loss_weight, device, lr, gan_type=gan_type, nrb_high=nrb_high, nrb_low=nrb_low, nrb_top=nrb_top,levels=[0,1,2],weights=[0.5,0.3,0.2])
+    # summary(lptn_model.net_g , input_size=(3, 608, 896))
+    total_loss = []
+    psnr_test,ssim_test, lpips_test,mssim_test = 0,0,0,0
+
+ 
+    with tqdm(
+        test_loader
+    ) as loader:
+
+        lptn_model.load_network(path, device=device)
+        for iteration,batch_data in enumerate(loader):
+            x,y = batch_data
+            
+            lptn_model.net_g.eval()
+            
+            lptn_model.feed_data(x,y)
+            
+            psnr_test_iter,ssim_test_iter, lpips_test_iter,mssim_iter_test = lptn_model.optimize_parameters(iteration,mode='test')
+            # lptn_model.visualise(iteration=iteration)
+            flag = 0
+            
+            lpips_test += lpips_test_iter
+            psnr_test += psnr_test_iter
+            ssim_test += ssim_test_iter
+            mssim_test+=mssim_iter_test
+    lpips_test /=(iteration+1)
+    psnr_test /= (iteration+1)
+    ssim_test /= (iteration+1)
+    mssim_test /= (iteration+1)
+
+    print(f'TEST LPIPS mix {lpips_test}')
+    print(f'TEST PSNR mix  {psnr_test}')
+    print(f'TEST SSIM mix {ssim_test}')
+    print(f'TEST MSSIM mix {mssim_test}')
 
     # test_dataset = SICEGradTest(
     #     root_dir=r'/kaggle/input/sice-grad-and-sice-mix/SICEGM',
