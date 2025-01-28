6f96902622fa5080008ac62b7898dca726748af2
sanskritilabroo <sanskritilabroo@gmail.com>
2025-01-29 00:42:16 +0530

    hi

--- src/utils/models/lptn_model.py
+++ src/utils/models/lptn_model.py
@@ -47,7 +47,7 @@ class LPTNModel(BaseModel):
         model = LPTN(
         nrb_low =self.nrb_low,
         nrb_high =self.nrb_high,
-        nrb_top =self.nrb_top,
+        #nrb_top =self.nrb_top,
         num_high= self.num_high,
         device=self.device,
         )
